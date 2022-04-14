e0a97a133a00fb0e299d09943fe27c8422685d3d
chiaraperuzzi <chiara.peruzzi@mail.polimi.it>
2022-04-14 09:58:13 +0200

    generate deck assistant

--- src/main/java/it/polimi/ingsw/model/assistant/DeckAssistant.java
+++ src/main/java/it/polimi/ingsw/model/assistant/DeckAssistant.java
@@ -15,16 +15,20 @@ public class DeckAssistant {
     }
 
     public void generateAssistantDeck(){
-        cardsInHand.add(new AssistantCard("nome", 1, 9));
-        // altre carte
+        cardsInHand.add(new AssistantCard("lion", 1, 1));
+        cardsInHand.add(new AssistantCard("ostrich", 1, 2));
+        cardsInHand.add(new AssistantCard("cat", 2, 3));
+        cardsInHand.add(new AssistantCard("falcon", 2, 4));
+        cardsInHand.add(new AssistantCard("fox", 3, 5));
+        cardsInHand.add(new AssistantCard("lizard", 3, 6));
+        cardsInHand.add(new AssistantCard("octopus", 4, 7));
+        cardsInHand.add(new AssistantCard("dog", 4, 8));
+        cardsInHand.add(new AssistantCard("elephant", 5, 9));
+        cardsInHand.add(new AssistantCard("turtle", 5, 10));
     }
 
-   /* public ArrayList<AssistantCard> updateDeck() {
+   /** public ArrayList<AssistantCard> updateDeck() {
         return this.cardsInHand;
     }  //ritorna il mazzo di carte senza la carta selezionata     */
-
-    /* public boolean checkIsEmpty() {
-        return cardsInHand.toArray().length != 0;
-    } */
 
 }
