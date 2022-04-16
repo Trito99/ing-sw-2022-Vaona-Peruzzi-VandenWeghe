a3fee3a5f841dfb063e847e9aef7b1f65814f238
federicoVaona <federico.vaona@mail.polimi.it>
2022-04-16 17:14:33 +0200

    sistemato costi charactercard

--- src/main/java/it/polimi/ingsw/model/character/DeckCharacter.java
+++ src/main/java/it/polimi/ingsw/model/character/DeckCharacter.java
@@ -21,17 +21,17 @@ public class DeckCharacter {
 
     public void generateCharacterDeck(){
         characterCards.add(new CharacterCard(1, CardEffect.BACCO));
-        characterCards.add(new CharacterCard(1, CardEffect.CICCIOPANZA));
-        characterCards.add(new CharacterCard(1, CardEffect.ALZABANDIERA));
+        characterCards.add(new CharacterCard(2, CardEffect.CICCIOPANZA));
+        characterCards.add(new CharacterCard(3, CardEffect.ALZABANDIERA));
         characterCards.add(new CharacterCard(1, CardEffect.MAILMAN));
-        characterCards.add(new CharacterCard(1, CardEffect.OLDLADY));
-        characterCards.add(new CharacterCard(1, CardEffect.TAURO));
+        characterCards.add(new CharacterCard(2, CardEffect.OLDLADY));
+        characterCards.add(new CharacterCard(3, CardEffect.TAURO));
         characterCards.add(new CharacterCard(1, CardEffect.JOKER));
-        characterCards.add(new CharacterCard(1, CardEffect.KNIGHT));
-        characterCards.add(new CharacterCard(1, CardEffect.FUNGAIOLO));
+        characterCards.add(new CharacterCard(2, CardEffect.KNIGHT));
+        characterCards.add(new CharacterCard(3, CardEffect.FUNGAIOLO));
         characterCards.add(new CharacterCard(1, CardEffect.MENESTRELLO));
-        characterCards.add(new CharacterCard(1, CardEffect.DAME));
-        characterCards.add(new CharacterCard(1, CardEffect.THIEF));
+        characterCards.add(new CharacterCard(2, CardEffect.DAME));
+        characterCards.add(new CharacterCard(3, CardEffect.THIEF));
 
     }
 
