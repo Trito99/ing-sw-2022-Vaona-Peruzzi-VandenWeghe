3c53eb256fcf3c0cabf615250d273371a17e2bf0
chiaraperuzzi <chiara.peruzzi@mail.polimi.it>
2022-04-20 15:52:43 +0200

    continuo implementazione CLIENT-SERVER - metodo di ObservableView (observerView da sviluppare con CLI) - metodi in View (da completare) - metodo reconnect in Lobby - metodo handleMessage in ClientHandler

--- src/main/java/it/polimi/ingsw/view/View.java
+++ src/main/java/it/polimi/ingsw/view/View.java
@@ -10,4 +10,14 @@ public interface View {
 
     void showLogin(String username, String gameId, boolean wasJoined);
 
+    /** mostra un messaggio generico */
+    void showMessage(String message);
+
+    void showPlayerInfluence(int influence);
+
+    void showPlayerTurn(String activePlayer);
+    
+    void showPlayerList(ArrayList<String> playerOrder);
+
+
 }
