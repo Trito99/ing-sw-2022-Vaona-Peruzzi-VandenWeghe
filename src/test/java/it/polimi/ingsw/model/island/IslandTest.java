a81809496281a1752a13ea1f065dc52e9fb0f006
chiaraperuzzi <chiara.peruzzi@mail.polimi.it>
2022-04-25 10:25:56 +0200

    Test InfluenceOnIsland in Island Test

--- src/test/java/it/polimi/ingsw/model/island/IslandTest.java
+++ src/test/java/it/polimi/ingsw/model/island/IslandTest.java
@@ -1,6 +1,7 @@
 package it.polimi.ingsw.model.island;
 
 import it.polimi.ingsw.model.assistant.DeckAssistant;
+import it.polimi.ingsw.model.character.CardEffect;
 import it.polimi.ingsw.model.game.GameMode;
 import it.polimi.ingsw.model.player.Player;
 import it.polimi.ingsw.model.player.PlayerNumber;
@@ -12,17 +13,24 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 public class IslandTest {
    private Table table;
+   private ArrayList<Player> players;
 
    @BeforeEach
    public void setup(){
       table = new Table();
+      players = new ArrayList<>();
       table.addFinalStudents();
       table.generateIslandCards();
       table.generateMotherEarth();
+
+      players.add(new Player(TColor.WHITE, PlayerNumber.PLAYER1));
+      players.add(new Player(TColor.BLACK, PlayerNumber.PLAYER2));
    }
 
    @Test
@@ -60,7 +68,7 @@ public class IslandTest {
 
    @Test
    public void MotherEarthOnIsland(){
-      int count = 0;
+      int count = 0; //ripete su tutte le isole
 
       for(count = 0; count < 12; count++) {
          IslandCard island = new IslandCard(count);
@@ -77,8 +85,8 @@ public class IslandTest {
    @ParameterizedTest
    @EnumSource(TColor.class)
    public void TowerOnIsland(TColor color){
-      int count = 0;
-      int i = 0;
+      int count = 0; //ripete su tutte le isole
+      int i = 0; //indice delle torri in TowerZone
       Player player = new Player(color, PlayerNumber.PLAYER1);
       GameMode gm = GameMode.TWOPLAYERS;
       player.generateSchool(table, gm);
@@ -113,8 +121,14 @@ public class IslandTest {
       for(count = 0; count < 12; count++){
          IslandCard island = new IslandCard(count);
 
+         players.get(0).setInfluenceOnIsland(6);
+         players.get(1).setInfluenceOnIsland(3);
+         /** DA CONTROLLARE: la funzione deve poter funzionare anche senza carta effetto*/
+         Player winner = island.calculateInfluence(players, CardEffect.CORTIGIANA);
+         assertNotNull(winner);
+         assertEquals(players.get(0), winner);
+         assertEquals(6, winner.getInfluenceOnIsland());
       }
-      return;
    }
 
    /** da fare */
