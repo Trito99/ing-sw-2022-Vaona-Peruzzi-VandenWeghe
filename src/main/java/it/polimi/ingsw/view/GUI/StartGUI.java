e82db9130fbb569df7624ebd98dcc9cf18061a49
chiaraperuzzi <chiara.peruzzi@mail.polimi.it>
2022-05-21 14:38:00 +0200

    java fx

--- src/main/java/it/polimi/ingsw/view/GUI/StartGUI.java
+++ src/main/java/it/polimi/ingsw/view/GUI/StartGUI.java
@@ -1,5 +1,18 @@
 package it.polimi.ingsw.view.GUI;
 
-public class StartGUI {
+import it.polimi.ingsw.network.ClientMessanger;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+public class StartGUI extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        GUI view = new GUI();
+        ClientMessanger clientMessanger = new ClientMessanger(view);
+        view.addObserver(clientMessanger);
+        //...
+    }
 
 }
--- src/main/java/it/polimi/ingsw/view/GUI/scene/ConnectToServer.java
+++ src/main/java/it/polimi/ingsw/view/GUI/scene/ConnectToServer.java
@@ -2,7 +2,7 @@ package it.polimi.ingsw.view.GUI.scene;
 
 import it.polimi.ingsw.observer.ObservableView;
 
-/** gestisce la connessione al server */
+/** scena che gestisce la connessione al server */
 
 public class ConnectToServer extends ObservableView implements GenericScene {
 }
