4d73b57d6edd8fe8b2337046decadd9ad61a13c9
federicoVaona <federico.vaona@mail.polimi.it>
2022-06-15 10:18:20 +0200

    test effects

--- src/main/java/it/polimi/ingsw/model/table/Table.java
+++ src/main/java/it/polimi/ingsw/model/table/Table.java
@@ -156,7 +156,7 @@ public class Table implements Serializable {
     public void generateCharacterCardsOnTable(ArrayList<CharacterCard> characterCards){
         //Collections.shuffle(characterCards);
 
-        for( int i = 0; i<3; i++){
+        for( int i = 6; i<9; i++){
             characterCardsOnTable.add(characterCards.get(i));
         }
 
