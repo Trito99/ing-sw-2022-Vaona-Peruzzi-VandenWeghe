65a463340127299f52238d6a2f0a0838c47c77e2
federicoVaona <federico.vaona@mail.polimi.it>
2022-06-20 20:25:35 +0200

    Logo app

--- src/main/java/it/polimi/ingsw/view/GUI/StartGUI.java
+++ src/main/java/it/polimi/ingsw/view/GUI/StartGUI.java
@@ -6,6 +6,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -37,7 +38,8 @@ public class StartGUI extends Application {
         stage.setScene(scene);
 
         stage.setTitle("Eriantys");
-        //stage.getIcons().add("/images/Logo_Eriantys.png");
+        Image icon = new Image(getClass().getResourceAsStream("/images/Logo_Eriantys.png"));
+        stage.getIcons().add(icon);
         stage.setResizable(true);
         stage.setMaximized(false);
         stage.setFullScreen(false);
