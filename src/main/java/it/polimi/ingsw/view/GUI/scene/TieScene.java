517c476bfbabe77335654feaaae23be8ab122535
chiaraperuzzi <chiara.peruzzi@mail.polimi.it>
2022-07-01 11:07:11 +0200

    bug fixes

--- src/main/java/it/polimi/ingsw/view/GUI/scene/TieScene.java
+++ src/main/java/it/polimi/ingsw/view/GUI/scene/TieScene.java
@@ -18,6 +18,7 @@ public class TieScene extends ObservableView implements GenericScene {
     /**
      * Initialize buttons present in the Scene
      */
+    @FXML
     public void initialize(){
         closeButton.addEventHandler(MouseEvent.MOUSE_CLICKED, this::buttonClose);
     }
